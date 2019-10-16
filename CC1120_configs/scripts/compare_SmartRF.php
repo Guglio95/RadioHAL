9cb3a3a6ec879544b3174bcba4400086d2fa337a
Andrea Guglio <il.guglio95@gmail.com>
2019-10-17 00:30:14 +0200

    compare_SmartRF.php now generates RFQuack commands to set registers

--- CC1120_configs/scripts/compare_SmartRF.php
+++ CC1120_configs/scripts/compare_SmartRF.php
@@ -3,7 +3,7 @@
 # This script is helpful to compare 2 .xml representation from SmartRF in order to identify differences
 
 if (!isset($argv[1]) || !isset($argv[2]) || !file_exists($argv[1]) || !file_exists($argv[2]))
-die("Tool used to compare 2 xml representation obtained from SmartRF\nusage ./script.php file1.xml file2.xml\n");
+die("Tool used to compare 2 xml representation obtained from SmartRF\nusage ./script.php file1.xml file2.xml [print RFQuack commands]\n");
 
 $filename1 = $argv[1];
 $filename2 = $argv[2];
@@ -70,5 +70,19 @@ foreach ($differences as $regName => $value) {
     echo str_pad("missing",$maxLen);
   }
   echo "\n";
+}
+echo "\n";
+
+if (isset($argv[3])){
+  include("register_names.php");
+
+  echo "RFQuack commands in order to apply $filename1 starting from $filename2\n\n";
+  foreach ($differences as $regName => $value) {
+    if (isset($file1[$regName])){
+      echo "q.set_register(0x".dechex($reg[$regName]).", 0x".dechex($file1[$regName])."); time.sleep(0.4); #".$regName."\n";
+    }
+  }
+}else{
+  echo "If you need RFQuack commands in order to apply this configuration look at help.\n";
 }
 echo "\n";
